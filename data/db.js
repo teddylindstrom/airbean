a289552106c5da4a8ccd065f92cc88de1b9a910f
floraohrman <floraohrman@gmail.com>
2026-04-02 12:36:38 +0000

    batabase update for orders and order history

--- data/db.js
+++ data/db.js
@@ -20,5 +20,28 @@ db.exec(`
   )
 `);
 
+// Orders
+db.exec(`
+  CREATE TABLE IF NOT EXISTS orders (
+    id TEXT PRIMARY KEY,
+    user_id TEXT,
+    total_price INTEGER,
+    status TEXT,
+    createdAt TEXT,
+    FOREIGN KEY(user_id) REFERENCES users(id)
+  )
+`);
+
+// Order items (vad som finns i ordern)
+db.exec(`
+  CREATE TABLE IF NOT EXISTS order_items (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    order_id TEXT,
+    product_id TEXT,
+    quantity INTEGER,
+    price INTEGER,
+    FOREIGN KEY(order_id) REFERENCES orders(id)
+  )
+`);
 
 export default db;
