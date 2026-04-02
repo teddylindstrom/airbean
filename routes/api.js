132e02f42efb69e132e345d75c44d0bb2f6d4e90
floraohrman <floraohrman@gmail.com>
2026-04-02 13:20:39 +0000

    orderRoutes and history history added - Flora

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import productsRoutes from "./productRoutes.js"
 import userRoutes from "./userRoutes.js"
+import orderRoutes from "./orderRoutes.js"
 
 const router = Router()
+
 router.use("/menu",productsRoutes)
 router.use("/user",userRoutes)
-
+router.use("/orders",orderRoutes)
 
 export default router;
 
